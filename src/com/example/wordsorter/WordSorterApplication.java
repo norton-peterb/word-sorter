ec81dc1ef0c33dbcf6311aeac370ad27d370a70c
Peter B Norton <norton.peterb@gmail.com>
2023-03-19 10:06:38 +0000

    Add Mechanims for sorting parameters

--- src/com/example/wordsorter/WordSorterApplication.java
+++ src/com/example/wordsorter/WordSorterApplication.java
@@ -1,5 +1,7 @@
 package com.example.wordsorter;
 
+import java.util.*;
+
 public class WordSorterApplication implements Runnable {
 
     private final String[] arguments;
@@ -13,8 +15,33 @@ public class WordSorterApplication implements Runnable {
         application.run();
     }
 
+    private List<String> processDuplicates(List<String> inputList) {
+        Map<String,String> duplicationMap = new HashMap<>();
+        inputList.forEach(
+                input -> {
+                    if(!duplicationMap.containsKey(input.toLowerCase())) {
+                        duplicationMap.put(input.toLowerCase(),input);
+                    }
+                }
+        );
+        return new ArrayList<>(duplicationMap.values());
+    }
+
     @Override
     public void run() {
-
+        List<String> args = new ArrayList<>(List.of(arguments));
+        args = processDuplicates(args);
+        args.sort(String::compareToIgnoreCase);
+        args.sort(Comparator.comparing(String::length));
+        StringBuilder stringBuilder = new StringBuilder();
+        args.forEach(
+                arg -> {
+                    if(!stringBuilder.toString().equals("")) {
+                        stringBuilder.append(",");
+                    }
+                    stringBuilder.append(arg);
+                }
+        );
+        System.out.println(stringBuilder);
     }
 }
